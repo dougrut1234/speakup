70851e9ed93969be72e8a20cd51cf5431d57db59
Leila Mafoud <leilamafoud@gmail.com>
2017-05-10 18:14:13 -0400

    added data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "dougrut1234@aol.com", password: "123456")
+Event.create(address1: "30 Wyckoff Ave", address2: "", city: "Ramsey", state: "New Jersey", zip:"07446", time_date: "5:00", title: "HEALTHCARE REFORM", description: "Talk to your congressman about your health bills")
