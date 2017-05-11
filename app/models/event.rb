fa09d688cb7c2db0821f4344349125a6ba419561
Doug Rutnarak <dougrut1234@aol.com>
2017-05-11 11:59:24 -0400

    event styling

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
 	has_many :users
 
+	validates :zip, length: {is: 5}, presence: true
+
 def self.search(search)
  where("city LIKE ?", "%#{search}%")
  where("zip LIKE ?", "%#{search}%")
